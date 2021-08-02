fd67dd1be96437034aab92444b10129704be3bfd
zachlim98 <68678549+zachlim98@users.noreply.github.com>
2021-08-02 23:31:37 +0800

    Updated weather info

--- sgx-weather/main.R
+++ sgx-weather/main.R
@@ -11,19 +11,30 @@ STI_returns <- STI_csv %>% tq_transmute(select = Close, mutate_fun = periodRetur
 # read PSI data 
 psi <- read.csv("psi.csv") %>% 
   mutate(Date = as.Date(psi$Date, format = "%d/%m/%Y"))
+# read weather data
+weather <- read.csv("merged.csv") %>% 
+  mutate(Date = as.Date(weather$Date, format = "%Y-%m-%d"))
 
 # join to create new df with returns, volume, PSI
 STI_vol <- STI_csv %>% select("Date", "Volume") %>% mutate(Volume = Volume/100000)
 df <- 1
-df <- inner_join(psi, STI_returns, by = "Date") 
-df <- inner_join(df, STI_vol, by = "Date")
+df <- psi %>% inner_join(STI_returns, by = "Date") %>% 
+  inner_join(., STI_vol, by = "Date") %>% inner_join(., weather, by = "Date")
 df_logit <- df %>% mutate(psi_ma = rollapply(PSI, 21, mean, fill=NA),
                           vol_ma = rollapply(Volume, 21, mean, fill=NA)) %>%
   na.omit() %>% mutate(psi_high = ifelse(PSI > psi_ma, 1, 0),
                        vol_high = ifelse(Volume > vol_ma, 1, 0),
                        stock_bi = ifelse(returns > 0 ,1, 0)) 
 
-model <- lm(Volume ~ PSI + Date, data = df_logit)
+# dummy encoding psi levels
+df_dummy <- df %>% mutate(psi_good = ifelse(PSI < 51, 1, 0),
+                          psi_norm = ifelse(PSI >50 & PSI < 100, 1, 0),
+                          psi_unhealthy = ifelse(PSI > 100 & PSI < 301, 1, 0),
+                          psi_hazard = ifelse(PSI > 300, 1, 0),
+                          positive = ifelse(returns > 0, 1, 0)) %>% 
+  select(!c(X, PSI, psi_hazard))
+
+model <- glm(returns ~ psi_good + psi_norm + psi_unhealthy + AvgTemp + SumRain + AvgWind + Volume, data = df_dummy)
 summary(model)
 plot(df$PSI, df$Volume, xlab = "PSI", ylab="Trade Volume (00,000)", col="red")
 abline(lm(df$Volume ~ df$PSI), col = "blue")
